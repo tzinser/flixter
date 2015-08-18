ea7ac56102bc8af975075380d59b7552901a0e08
Tina Zinser <zinsert7@gmail.com>
2015-08-18 00:56:59 +0000

    Lesson 13, Adding validation for new course form

--- test/controllers/instructor/courses_controller_test.rb
+++ test/controllers/instructor/courses_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class Instructor::CoursesControllerTest < ActionController::TestCase
+  #Test to make sure the course detail page is properly displayed  
   test "course show page" do 
   	  user = FactoryGirl.create(:user)
   	  sign_in user
@@ -8,5 +9,17 @@ class Instructor::CoursesControllerTest < ActionController::TestCase
    	  course = Course.create(:title => "Test Course", :description => "A sample course for testing", :cost => "25.00")
    	  get :show, :id => course.id
    	  assert_response :success
+   end
+
+   # Test to make sure the user must be signed in to create a new course
+   test "create not signed in" do 
+   		assert_no_difference 'Course.count' do 
+   			post :create, :course => {
+   				:title => "Test Course", 
+   				:description => "A sample course for testing", 
+   				:cost => "25.00", 
+   				}
+   			end
+   		 assert_redirected_to new_user_session_path
    end
 end
