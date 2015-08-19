3253364700ff84d7ec1970383544b6fa0f292d9a
Tina Zinser <zinsert7@gmail.com>
2015-08-19 21:43:57 +0000

    Lesson 16, create sections controller and model, set routes and relationships, build form for new sections, have sections display on course show page

---
+++ db/migrate/20150819204600_create_sections.rb
@@ -0,0 +1,9 @@
+class CreateSections < ActiveRecord::Migration
+  def change
+    create_table :sections do |t|
+      t.string :title
+      t.integer :course_id
+      t.timestamps
+    end
+  end
+end
