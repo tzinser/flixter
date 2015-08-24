9aa15f85d9ec5cab92d624dee3b4a2f3f6322c67
Tina Zinser <zinsert7@gmail.com>
2015-08-24 17:07:41 +0000

    Lesson 28 adding gem and configure for stripe to process payments

--- Gemfile
+++ Gemfile
@@ -67,3 +67,6 @@ gem 'mini_magick'
 
 #video integration
 gem 'carrierwave-aws'
+
+#payment processing
+gem 'stripe'
--- app/models/course.rb
+++ app/models/course.rb
@@ -9,6 +9,13 @@ class Course < ActiveRecord::Base
 	validates :description, :presence => true
 	validates :cost, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
 
+	def free?
+		cost.zero?
+	end
+
+	def premium?
+		! free?
+	end
 	# def total_cost
 	# 	cost * 1.10
 	# end
