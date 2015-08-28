ae3bb4509544ceab5b2789521bfabdfb16edcead
Tina <zinsert7@gmail.com>
2015-08-28 14:35:21 +0000

    Lesson 38, finalize flixter site.  Adding static pages and link from footer

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Flixter::Application.routes.draw do
   # You can have the root of your site routed with "root"
   resource :dashboard, :only =>[:show]
   root 'static_pages#index'
+  get 'privacy', :to => 'static_pages#privacy'
+  get 'team', :to => 'static_pages#team'
+  get 'careers', :to => 'static_pages#careers'
   resources :courses, :only => [:index, :show] do
     resources :enrollments, :only => :create
   end
